eb3bafafcce46c7a0cdaf2e610d0e5b01c85a92f
Brendan Cordova <cordovalegacy19@gmail.com>
2023-05-23 14:19:20 -0700

    Added Loading Effect

--- src/app/components/Results.jsx
+++ src/app/components/Results.jsx
@@ -2,12 +2,21 @@
 
 const Results = ({ res }) => {
     return (
-        <main className="flex min-h-screen flex-col items-center gap-2 p-24">
-            <h1 className='font-bold'>Home</h1>
+        <main className="flex min-h-screen flex-col items-center gap-2 p-10">
+            <h1 className='font-bold text-2xl mb-10'>Home</h1>
             <div className="flex flex-wrap justify-evenly items-center">
                 {res.map((movies) => (
-                    <div className="flex flex-col border bg-gray-800 p-2 rounded-md">
-                        <h2>{movies.title}</h2>
+                    <div
+                    key={movies.id} 
+                    className="flex flex-col mb-4 justify-start border p-2 rounded-md w-96 h-80 text-center">
+                        <div>
+                            <h2>{movies.original_title ? movies.original_title : movies.name}</h2>
+                            <p>Release Date: {movies.release_date}</p>
+                            <p>Average Movie Rating: {movies.vote_average}</p>
+                        </div>
+                        <div className="mt-4 overflow-auto p-2 text-center">
+                            <p>{movies.overview}</p>
+                        </div>
                     </div>
                 ))
                 }
---
+++ src/app/loading.jsx
@@ -0,0 +1,15 @@
+
+
+const Loading = () => {
+    return(
+        <div className="flex justify-center">
+            <img 
+            src="spinner.svg" 
+            alt="loading icon"
+            className="w-96" 
+            />
+        </div>
+    )
+}
+
+export default Loading
