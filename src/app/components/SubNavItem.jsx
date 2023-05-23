b897290aa42d35c1c3bf325fc0f3e0a0cf177505
Brendan Cordova <cordovalegacy19@gmail.com>
2023-05-23 10:28:53 -0700

    Finished Nav Component

--- src/app/components/SubNavItem.jsx
+++ src/app/components/SubNavItem.jsx
@@ -1,11 +1,22 @@
+"use client"
+
 import Link from "next/link"
+import { useSearchParams } from 'next/navigation' //client only
 
-const SubNavItem = ({title, params}) => {
+const SubNavItem = ({ title, params }) => {
 
-    return(
-    <>
-        <Link href={`/?genre=${params}`} className="hover:text-amber-300 transition">{title}</Link>
-    </>
+    const searchParams = useSearchParams() //creates instance
+    const genre = searchParams.get("genre") //create key and value pair {genre: params}
+
+    return (
+        <>
+            <Link
+                href={`/?genre=${params}`}
+                className={`${genre && genre === params ? "underline": ""}
+                hover:text-amber-500 transition duration-300 font-semibold p-2 ${params === "fetchTopRated" || params === "fetchTrending" ? "hover:underline rounded-lg underline-offset-8 decoration-4 decoration-amber-500" : ""}`}>
+                {title}
+            </Link>
+        </>
     )
 }
 
